fa5f832736a9609ffd8edf30f327ee6224a8bc1e
hophoet <hophoet@gmail.com>
2021-08-11 23:01:45 +0000

    (database) setup mongodb database

---
+++ src/database/setup.py
@@ -0,0 +1,18 @@
+import motor.motor_asyncio
+from fastapi_users.db import MongoDBUserDatabase
+# from api.models.user import UserDB
+from src.models.user import UserDB
+
+# DATABASE_URL = "mongodb://localhost:27017"
+PASSWORD = "dCzClVpE6BMOeeWX"
+DATABASE_URL = "mongodb://localhost:27017"
+# DATABASE_URL=f"mongodb://hophoet:{PASSWORD}@livrimdb-shard-00-00.p0thu.mongodb.net:27017,livrimdb-shard-00-01.p0thu.mongodb.net:27017,livrimdb-shard-00-02.p0thu.mongodb.net:27017/livrimdb?ssl=true&replicaSet=atlas-s5l1sw-shard-0&authSource=admin&retryWrites=true&w=majority"
+DATABASE_NAME = 'livrimdb'
+SECRET = "SECRET"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    DATABASE_URL, uuidRepresentation="standard"
+)
+db = client["yeyadb"]
+collection = db["users"]
+user_db = MongoDBUserDatabase(UserDB, collection)

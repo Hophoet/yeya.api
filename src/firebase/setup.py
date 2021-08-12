6084142f15e15c3c58e22e834b7d2421a4499434
hophoet <hophoet@gmail.com>
2021-08-12 01:39:31 +0000

    (firebase) setup firebase

---
+++ src/firebase/setup.py
@@ -0,0 +1,38 @@
+import os
+import pyrebase
+from .fake import (
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_AUTH_PASSWORD, 
+  FIREBASE_AUTH_USERNAME, 
+  FIREBASE_DATABASE_URL, 
+  FIREBASE_STORAGE_BUCKET
+)
+
+
+# configs
+config = {
+  "apiKey": FIREBASE_API_KEY,
+  "authDomain": FIREBASE_AUTH_DOMAIN,
+  "databaseURL": FIREBASE_DATABASE_URL,
+  "storageBucket": FIREBASE_STORAGE_BUCKET
+}
+
+try:
+  firebase = pyrebase.initialize_app(config)
+  # storage
+  storage = firebase.storage()
+  # auth user
+  auth = firebase.auth()
+  user = auth.sign_in_with_email_and_password(
+    FIREBASE_AUTH_USERNAME,
+    FIREBASE_AUTH_PASSWORD
+  )
+except Exception as error:
+  print('Firebase setup error', error)
+  storage = None
+  user = None
+  auth= None
+
+
+

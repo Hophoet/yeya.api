3cab76834f508bbd83cfdefccdea0a3cd913b4b1
hophoet <hophoet@gmail.com>
2021-08-13 14:49:45 +0000

    (firebase) update upload to handle the image storage on the old image name if this one exist to avoid to crush the old image instead of store a new image

--- src/firebase/functions.py
+++ src/firebase/functions.py
@@ -1,17 +1,18 @@
 import uuid
-from src.firebase.setup import (storage, auth, user)
+from src.firebase.setup import get_firebase_objects 
 
-def upload(file):
+
+def upload(file, old_file_name):
     try:
-        file_name = str(uuid.uuid1())
-        store = storage.child(f'books/{file_name}.jpg').put(
+        auth, user, storage = get_firebase_objects()
+        file_name = old_file_name if old_file_name else str(uuid.uuid1())
+        store = storage.child(f'users/{file_name}.jpg').put(
             file,
             user['idToken']
         )
-        url = storage.child(f'books/{file_name}.jpg').get_url(
+        url = storage.child(f'users/{file_name}.jpg').get_url(
             store.get('downloadTokens')
         )
-        return url
+        return (url, file_name)
     except Exception as error:
         raise error
-        print('ERROR', error)

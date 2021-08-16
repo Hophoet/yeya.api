c704ab0ac7352701f5b9fbe13245a591616757b1
hophoet <hophoet@gmail.com>
2021-08-16 01:04:30 +0000

    (model manager) update UserManager add create_password_reset to handle password reset code setting

--- src/models/managers/user.py
+++ src/models/managers/user.py
@@ -4,11 +4,13 @@ from uuid import UUID
 import fastapi_users
 from src.database.manager import DBManager
 from src.models.category import Category, CategoryDB
-from src.models.user import User
+from src.models.user import (User, PasswordResetManagerData, PasswordResetDB)
 from src.models.image import Image
 from fastapi.encoders import jsonable_encoder
-from pydantic import UUID4
+from src.database.setup import user_db
 from bson import ObjectId
+from pydantic import UUID4, EmailStr
+import pdb
 
 class UserManager(DBManager):
     """ Category model requests manager """
@@ -38,13 +40,8 @@ class UserManager(DBManager):
     async def get_user(self, user_id:UUID4) -> User:
         """ get geolocation by id request """
         await self.connect_to_database()
-        print('USER ID', user_id)
-        self.db['users'].create_index("id", unique=True)
-        user_db = await self.db['users'].find_one({
-            'id': user_id
-        })
-        print('USER GETTED',user_db)
-        return user_db if user_db else None
+        user_q = await user_db.get(id=UUID(user_id))  
+        return user_q
 
     async def get_user_by_email(self, email:str) -> User:
         """ get user by id request """
@@ -52,3 +49,28 @@ class UserManager(DBManager):
         user_db = await self.db['users'].find_one({'email':email})
         if user_db:
             return user_db
+
+    async def create_password_reset(self, data:PasswordResetManagerData) -> User:
+        """ create password reset  request """
+        await self.connect_to_database()
+        # create the password reset db object
+        password_reset_db:PasswordResetDB = PasswordResetDB(
+            user_id=data.user_id,
+            code=data.code
+        )
+        password_reset_q = await self.db['passwordResets'].find_one({'user_id':data.user_id})
+        # pdb.set_trace()
+        if password_reset_q:
+            update_result =  await self.db['passwordResets'].update_one({
+                '_id': ObjectId(password_reset_q['_id'])
+            },{ '$set': password_reset_db.dict() })
+
+            # update the old one
+        else:
+            # create a new one 
+            create_result =  await self.db['passwordResets'].insert_one(
+                password_reset_db.dict() 
+            )
+        return 
+
+

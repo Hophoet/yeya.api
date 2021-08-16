100f797178d2c581d31782010306f943b8f11c61
hophoet <hophoet@gmail.com>
2021-08-16 01:03:40 +0000

    (model) add user password reset feature model

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,9 @@
 from fastapi_users import FastAPIUsers, models
+from datetime import datetime
 from typing import Optional
+
+from pydantic.main import BaseModel
+from pydantic.networks import EmailStr
 from src.models.image import Image
 
 
@@ -19,3 +23,28 @@ class UserUpdate(User, models.BaseUserUpdate):
 
 class UserDB(User, models.BaseUserDB):
     pass
+
+class PasswordReset(BaseModel):
+    """ users password reset feature model """
+    user: User 
+    code: Optional[str] = None
+    verified: bool = False
+    created_at: Optional[datetime]
+
+class PasswordResetManagerData(BaseModel):
+    """ model for the model manager method parameter"""
+    user_id: str 
+    code: str
+    verified: bool = False
+    created_at:Optional[datetime] = datetime.now()
+
+class PasswordResetDB(BaseModel):
+    """ users password reset feature model """
+    user_id: str 
+    code: Optional[str] = None
+    verified: bool = False
+    created_at:Optional[datetime] = datetime.now()
+
+class PasswordResetSerializer(BaseModel):
+    """ users password reset feature model """
+    email: EmailStr 

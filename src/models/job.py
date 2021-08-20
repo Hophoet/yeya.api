33db7d9a88a89a027a744ae96182636f0016fcd9
hophoet <hophoet@gmail.com>
2021-08-20 18:41:57 +0000

    (model) add price & city to Job model

--- src/models/job.py
+++ src/models/job.py
@@ -7,11 +7,14 @@ from .user import User
 from .category import Category
 from pydantic import UUID4, BaseModel, EmailStr, validator
 from .geolocation import Geolocation, GeolocationCreate
+from .city import City
 
 class Job(BaseModel):
     id: str = None
     title: str
     description: str
+    price: Optional[int] = None
+    city: Optional[City] = None
     user: User
     category: Optional[Category] = None
     geolocation: Optional[Geolocation] = None
@@ -21,6 +24,8 @@ class JobBD(BaseModel):
     id: Optional[str] = None
     title: str
     description: str
+    price: int
+    city_id: str
     user_id: Optional[UUID4] = None
     category_id: Optional[str] = None
     geolocation_id : Optional[str] = None
@@ -29,12 +34,16 @@ class JobBD(BaseModel):
 class JobDBUpdate(BaseModel):
     title: str
     description: str
+    price: int
+    city_id: Optional[str]
     category_id: Optional[str]
     geolocation_id : Optional[str] = None
 
 class JobCreate(BaseModel):
     title: str
     description: str
+    price: int
+    city_id: Optional[str]
     category_id: Optional[str]
     geolocation: Optional[GeolocationCreate] = None
 
@@ -42,5 +51,7 @@ class JobUpdate(BaseModel):
     id: str
     title: str
     description: str
+    price: int
+    city_id: Optional[str]
     category_id: Optional[str]
     geolocation: Optional[GeolocationCreate] = None

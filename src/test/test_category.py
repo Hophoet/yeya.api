d505b993e979853c994552ca3c174c34c1fcb8ab
hophoet <hophoet@gmail.com>
2021-09-05 00:23:44 +0000

    (test) add test_category_creation to test category creation request

--- src/test/test_category.py
+++ src/test/test_category.py
@@ -19,4 +19,19 @@ def test_get_categories():
     assert response.status_code == status.HTTP_200_OK
 
 
-
+def test_category_creation():
+    user_auth_response:dict = get_user_auth_response()
+    user:dict = user_auth_response.get('user')
+    bearer_token:dict = user_auth_response.get('token')
+    bearer_token_value:str = bearer_token.get('value') 
+    response = client.post(
+        "api/v1/category/create", 
+        headers={
+            'Authorization': f'Bearer {bearer_token_value}',
+        },
+        json={
+            'name':'test category',    
+            'description':'test category description',    
+        }
+        )
+    assert response.status_code == status.HTTP_200_OK

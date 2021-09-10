8aa7fdd86dcac6f579ae8d1203a49217e7b80700
hophoet <hophoet@gmail.com>
2021-09-10 13:47:03 +0000

    (firebase) get firebase configs from the os env

--- src/firebase/setup.py
+++ src/firebase/setup.py
@@ -1,21 +1,25 @@
 import os
 import pyrebase
-from .fake import (
-  FIREBASE_API_KEY, 
-  FIREBASE_AUTH_DOMAIN, 
-  FIREBASE_AUTH_PASSWORD, 
-  FIREBASE_AUTH_USERNAME, 
-  FIREBASE_DATABASE_URL, 
-  FIREBASE_STORAGE_BUCKET
-)
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+class Envs:
+    FIREBASE_API_KEY = os.getenv('FIREBASE_API_KEY')
+    FIREBASE_AUTH_DOMAIN = os.getenv('FIREBASE_AUTH_DOMAIN')
+    FIREBASE_AUTH_PASSWORD = os.getenv('FIREBASE_AUTH_PASSWORD')
+    FIREBASE_AUTH_USERNAME = int(os.getenv('FIREBASE_AUTH_USERNAME'))
+    FIREBASE_DATABASE_URL = os.getenv('FIREBASE_DATABASE_URL')
+    FIREBASE_STORAGE_BUCKET = os.getenv('FIREBASE_STORAGE_BUCKET')
+
+
 
 
 # configs
 config = {
-  "apiKey": FIREBASE_API_KEY,
-  "authDomain": FIREBASE_AUTH_DOMAIN,
-  "databaseURL": FIREBASE_DATABASE_URL,
-  "storageBucket": FIREBASE_STORAGE_BUCKET
+  "apiKey": Envs.FIREBASE_API_KEY,
+  "authDomain": Envs.FIREBASE_AUTH_DOMAIN,
+  "databaseURL": Envs.FIREBASE_DATABASE_URL,
+  "storageBucket": Envs.FIREBASE_STORAGE_BUCKET
 }
 
 
@@ -27,8 +31,8 @@ def get_firebase_objects():
     # auth user
     auth = firebase.auth()
     user = auth.sign_in_with_email_and_password(
-      FIREBASE_AUTH_USERNAME,
-      FIREBASE_AUTH_PASSWORD
+      Envs.FIREBASE_AUTH_USERNAME,
+      Envs.FIREBASE_AUTH_PASSWORD
     )
     return (auth, user, storage)
 
